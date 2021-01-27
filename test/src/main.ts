c5d1fe2bf0bd0f39893bdb73838e67f9edb0402b
KelianB-IMTA <kelian.baert@imt-atlantique.net>
2021-01-27 10:34:55 +0100

    refactoring + added more tests

--- test/src/main.ts
+++ test/src/main.ts
@@ -11,26 +11,78 @@ const ADMIN_URL = "http://localhost:9876";
 const BASE_URL = "http://localhost:8080";
 
 async function createAndAuthenticateUser(): Promise<string | null> {
+    const verbose = false;
+
+    console.log("###### Creating user & credentials ######");
+
     // Create user
-    console.log("###### Creating user ######");
-    await request(ADMIN_URL + "/users/", "POST", {}, USER, undefined, true);
+    await request(ADMIN_URL + "/users/", "POST", {}, USER, undefined, verbose);
 
     // Create credentials
-    console.log("###### Creating credentials ######");
     const {status, password} = await request(
         ADMIN_URL + "/credentials/",
         "POST",
         {},
         {consumerId: USER.username, type: "basic-auth"},
         undefined,
-        true,
+        verbose,
     );
 
     if (status === 200) {
-        const key = USER.username + ":" + password;
-        return Buffer.from(key).toString("base64");
+        return Buffer.from(`${USER.username}:${password}`).toString("base64");
     }
     return null;
+}
+
+async function test(): Promise<string | null> {
+    const authKey = await createAndAuthenticateUser();
+    if (authKey) {
+        console.log("Authentication key:", authKey);
+        const movie = await getRandomMovie();
+
+        console.log("###### Testing service: pricing ######");
+
+        console.log(`Setting price to 20$ for movie '${movie.title}'`);
+        await setMoviePrice(authKey, movie.id, 20);
+
+        console.log(`Setting price to 10$ for '${movie.title}'`);
+        await setMoviePrice(authKey, movie.id, 10);
+
+        console.log("Verifying price...");
+        let resp = await getPrice(authKey, movie.id);
+        if (resp.status === 200) {
+            if (resp.price === 10) console.log("OK");
+            else return "The price is incorrect.";
+        } else return "An error was raised while fetching the price.";
+
+        console.log("###### Testing service: renting ######");
+
+        console.log(`Renting movie '${movie.title}'`);
+        await rentMovie(authKey, movie.id);
+
+        console.log("Verifying rental...");
+        resp = await getRentals(authKey);
+        if (resp.status === 200) {
+            const rentals = resp.rentals as {userId: string; movieId: string}[];
+            const rental = rentals.find((r) => r.movieId === movie.id);
+            if (rental) console.log("OK");
+            else return "Unable to find the rental";
+        } else return "An error was raised while fetching rentals.";
+
+        console.log("###### Testing service: streaming ######");
+
+        console.log(`Streaming movie '${movie.title}'`);
+        let stream = await streamMovie(authKey, movie.id);
+        console.log("Response:", stream);
+
+        const anotherMovie = await getRandomMovie();
+        console.log(`Streaming another movie: '${anotherMovie.title}'`);
+        stream = await streamMovie(authKey, anotherMovie.id);
+        console.log("Response:", stream);
+
+        return null;
+    }
+    return "Failed to authenticate.";
 }
 
 async function getRandomMovie() {
@@ -39,16 +91,29 @@ async function getRandomMovie() {
 }
 
 async function rentMovie(authKey: string, movieId: string) {
-    return request(BASE_URL + "/renting/rent", "POST", {}, {movieId}, authKey, true);
+    const verbose = false;
+    return request(BASE_URL + "/renting/rent", "POST", {}, {movieId}, authKey, verbose);
 }
 
-async function test(): Promise<void> {
-    const authKey = await createAndAuthenticateUser();
-    if (authKey) {
-        const movie = await getRandomMovie();
-        console.log(movie);
-        await rentMovie(authKey, movie.id);
-    }
+async function getRentals(authKey: string) {
+    const verbose = false;
+    return request(BASE_URL + "/renting/", "GET", {}, {}, authKey, verbose);
 }
 
-test();
+async function setMoviePrice(authKey: string, movieId: string, price: number) {
+    const verbose = false;
+    return request(BASE_URL + "/renting/price", "POST", {}, {movieId, price}, authKey, verbose);
+}
+
+async function getPrice(authKey: string, movieId: string) {
+    const verbose = false;
+    return request(BASE_URL + "/renting/price/" + movieId, "GET", {}, {}, authKey, verbose);
+}
+
+async function streamMovie(authKey: string, movieId: string) {
+    const verbose = false;
+    return request(BASE_URL + "/streaming/" + movieId, "GET", {}, {}, authKey, verbose);
+}
+
+const err = test();
+if (err != null) console.error(err);
